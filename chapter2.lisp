e04ceaf3f9b4b1bdc7150dc9ee607273d3f99301
Rainbowbox <twentyfivehours@163.com>
2016-10-17 23:20:22 +0800

    init repo

---
+++ chapter2.lisp
@@ -0,0 +1,17 @@
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+  ; arithmetic shift
+  (ash (+ *small* *big*) -1))
+
+(defun smaller()
+  ; subtracts 1 from the result
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+
