bab7c570e109839558cb76cca24bfbd77cf9f439
Rainbowbox <twentyfivehours@163.com>
2016-11-15 22:47:12 +0800

    Add chapter 7

---
+++ chapter7.lisp
@@ -0,0 +1,25 @@
+; Common Lisp will use more complex printing routines for printing data structures.
+; These routines will check to see if you've run into a previously seen cons cell
+(setf *print-circle* t)
+(defparameter foo '(1 2 3))
+; Circular List
+(setf (cdddr foo) foo)
+(print foo)
+
+; Association Lists or alist for short
+; An alist consists of key/value pairs stored in a list
+(defparameter *drink-order* '((bill . double-espresso)
+                              (lisa . small-drip-coffee)
+                              (john . medium-latte)))
+
+(assoc 'lisa *drink-order*)
+; (LISA . SMALL-DRIP-COFFEE)
+
+(push '(lisa . large-mocha-with-whipped-cream) *drink-order*)
+; ((LISA . LARGE-MOCHA-WITH-WHIPPED-CREAM)
+;  (BILL . DOUBLE-ESPRESSO)
+;  (LISA . SMALL-DRIP-COFFEE)
+;  (JOHN . MEDIUM-LATTE))
+
+(assoc 'lisa *drink-order*)
+; (LISA . LARGE-MOCHA-WITH-WHIPPED-CREAM)
